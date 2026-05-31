046f7ddfae6536d9d3367c7d3c5cbad41d55cb7a
Alex <alex@users.noreply.github.com>
2026-05-30 21:34:08 -0400

    Fix TypeScript CI: remove unused import in starProseLint test.

--- src/content/starProseLint.test.ts
+++ src/content/starProseLint.test.ts
@@ -4,7 +4,6 @@ import {
   countWords,
   detectSensoryModalities,
   validateBriefingOrdersProse,
-  validateNpcBookendProse,
   validateSensoryBeat,
   validateStarStructure,
 } from "./starProseLint";
